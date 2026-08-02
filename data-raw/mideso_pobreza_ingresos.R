c8397c6313e7ca129dc4a86e7180ca6a4c492443
bastianolea <bastianolea@gmail.com>
2026-08-02 11:46:53 -0400

    datos de pobreza comunal

---
+++ data-raw/mideso_pobreza_ingresos.R
@@ -0,0 +1,35 @@
+## code to prepare `mideso_pobreza_ingresos` dataset goes here
+# https://bidat.gob.cl/details/ficha/dataset/base-de-datos-pobreza-comunal-2024
+
+library(readxl)
+library(dplyr)
+library(janitor)
+library(territorial)
+
+pobreza_0 <- read_xlsx("data-raw/sae_ingresos_2024.xlsx", skip = 2) |>
+  clean_names()
+
+pobreza_1 <- pobreza_0 |>
+  select(
+    codigo_comuna = codigo,
+    poblacion = numero_de_personas_en_situacion_de_pobreza_de_ingresos
+  )
+
+pobreza_2 <- pobreza_1 |>
+  filter_out(is.na(poblacion)) |>
+  mutate(codigo_comuna = as.numeric(codigo_comuna)) |>
+  mutate(año = 2024, .before = poblacion)
+
+mideso_pobreza_ingresos <- pobreza_2 |>
+  territorial::contextualizar(codigo_comuna)
+
+# validar
+library(pointblank)
+
+mideso_pobreza_ingresos |>
+  expect_col_count_match(8) |>
+  expect_row_count_match(345) |>
+  expect_col_vals_not_null(everything())
+
+
+usethis::use_data(mideso_pobreza_ingresos, overwrite = TRUE)
---
+++ tests/testthat/test-mideso_pobreza_multi.R
@@ -0,0 +1,13 @@
+library(pointblank)
+
+test_that("conteo de columnas", {
+  expect_col_count_match(mideso_pobreza_multi, 8)
+})
+
+test_that("conteo de filas", {
+  expect_row_count_match(mideso_pobreza_multi, 345)
+})
+
+test_that("sin datos perdidos", {
+  expect_col_vals_not_null(mideso_pobreza_multi, everything())
+})
